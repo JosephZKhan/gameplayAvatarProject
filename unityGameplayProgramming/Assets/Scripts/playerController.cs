76ca8189d9cfcb7655fa54e30d3260bc91e99b51
joseph khan <joseph2.khan@live.uwe.ac.uk>
2022-02-15 13:07:07 +0000

    refined movement

--- unityGameplayProgramming/Assets/Scripts/playerController.cs
+++ unityGameplayProgramming/Assets/Scripts/playerController.cs
@@ -10,6 +10,8 @@ public class playerController : MonoBehaviour
     private float movementX;
     private float movementY;
     public float speed = 1;
+    private float maxVelocityX = 50000;
+    private float maxVelocityY = 50000;
 
     // Start is called before the first frame update
     void Start()
@@ -32,6 +34,20 @@ public class playerController : MonoBehaviour
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
-        
+
+        if (rb.velocity.x >= maxVelocityX)
+        {
+            Debug.Log("TOO FAST IN X");
+            rb.velocity = new Vector3(maxVelocityX, rb.velocity.y, rb.velocity.z);
+        }
+
+        if (rb.velocity.y >= maxVelocityY)
+        {
+            Debug.Log("TOO FAST IN Y");
+            rb.velocity = new Vector3(rb.velocity.x, maxVelocityY, rb.velocity.z);
+        }
+
+
+
     }
 }
